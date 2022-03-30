1ca7e11a0b636148444e660983459a7a51b2aa46
Jesse Antoni <i489617@fhict.nl>
2022-03-30 13:54:29 +0200

    added spotify, footer and article

---
+++ components/Footer.vue
@@ -0,0 +1,60 @@
+<template>
+  <section class="bg-rockstar-yellow p-4">
+    <div class="my-4">
+      <div class="container mx-auto px-16">
+        <div class="grid grid-cols-3 gap-8">
+          <div>
+            <Title size="4">
+              TRIBES
+            </Title>
+            <div class="ml-4">
+              <ul>
+                <li><a>Java Tribes</a></li>
+                <li><a>.Net Tribes</a></li>
+                <li><a>Mobile Tribes</a></li>
+                <li><a>All Tribes</a></li>
+              </ul>
+            </div>
+          </div>
+
+          <div>
+            <Title size="4">
+              LAATSTE ARTIKELEN
+            </Title>
+            <div class="ml-4">
+              <ul>
+                <li><a>Artikel naam</a></li>
+                <li><a>Artikel naam</a></li>
+                <li><a>Artikel naam</a></li>
+              </ul>
+            </div>
+          </div>
+
+          <div>
+            <Title size="4">
+              MEER INFORMATIE
+            </Title>
+            <div class="ml-4">
+              <ul>
+                <li><a>Contact</a></li>
+                <li><a>Merchandise</a></li>
+                <li><a>Privacy policy</a></li>
+              </ul>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+}
+</script>
+
+<style scoped>
+
+</style>
