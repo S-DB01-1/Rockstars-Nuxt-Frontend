28cc73b52594a077a56fd781a51bb45f639adf71
LukeSteegh <i484363@fhict.nl>
2022-03-31 14:49:25 +0200

    Added author description component

---
+++ components/AuthorDescription.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="container mx-auto lg:px-16 px-8">
+    <div class="bg-rockstar-yellow -mt-16 p-4">
+      <div class="grid md:grid-cols-1 lg:grid-cols-2 gap-4">
+        <div>
+          <Title size="5">
+            AUTHOR
+          </Title>
+          <Title class="underline" size="1">
+            PIETER JAN
+          </Title>
+          <Title size="6">
+            Hier komt de omschrijving van de autheur. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed tempus urna et pharetra pharetra massa massa ultricies mi.  Purus viverra accumsan in nisl nisi scelerisque. Nam libero justo laoreet sit amet cursus sit amet dictum. Feugiat in fermentum posuere urna nec tincidunt praesent semper. Massa placerat duis ultricies lacus sed turpis tincidunt.
+          </Title>
+          <br><br>
+        </div>
+
+        <div class="profileBox">
+          <div>
+            <img :src="require('assets/img/User.jpg')" class="avatar" />
+          </div>
+          <div>
+            <Title class="underline" size="1">
+              Tribe naam
+            </Title>
+            <Title size="3">
+              Software Developer
+            </Title>
+          </div>
+          <div class="flex justify-center space-x-2">
+            <img :src="require('assets/image/twitter.svg')" class="socials"/>
+            <img :src="require('assets/image/facebook.svg')" class="socials"/>
+            <img :src="require('assets/image/instagram.svg')" class="socials"/>
+            <img :src="require('assets/image/linkedin.svg')" class="socials"/>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'AuthorDescription'
+}
+</script>
+
+<style lang="scss">
+@import "assets/css/variables";
+
+.avatar {
+  border-radius: 50%;
+  width: 200px;
+  height: 200px;
+  object-fit: cover;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.profileBox {
+  margin: auto;
+  text-align: center;
+}
+
+.socials {
+  width: 7%;
+  height: 7%;
+}
+
+</style>
--- components/TribeDescription.vue
+++ components/TribeDescription.vue
@@ -1,53 +1,57 @@
 <template>
-<div class="content">
+  <div class="content">
 
     <div class="container mx-auto lg:px-16 px-8">
-        <div class="bg-rockstar-yellow -mt-16 p-4">
-            <div class="grid md:grid-cols-1 lg:grid-cols-2 gap-4">
-                <div>
-                    <Title size="5">
-                        Rockstar Tribes
-                    </Title>
-                    <Title size="1">
-                        JAVA TRIBE
-                    </Title>
-                    <Title size="6">
-                        Java, een van de populairste programmeertalen van het moment, is veel meer dan alleen dat. Het is een ecosysteem, een denkwijze, een opleidingsplan en bovenal: een hele actieve community. Java op zichzelf is niets; het zijn de ontwikkelaars die de taal hebben gemaakt tot wat het is. Precies dat wordt in de Java Tribe binnen Team Rockstars IT benadrukt.
-                    </Title>
-                    <br><br>
-                </div>
+      <div class="bg-rockstar-yellow -mt-16 p-4">
+        <div class="grid md:grid-cols-1 lg:grid-cols-2 gap-4">
+          <div>
+            <Title size="5">
+              Rockstar Tribes
+            </Title>
+            <Title size="1">
+              JAVA TRIBE
+            </Title>
+            <Title size="6">
+              Java, een van de populairste programmeertalen van het moment, is veel meer dan alleen dat. Het is een ecosysteem, een denkwijze, een opleidingsplan en bovenal: een hele actieve community. Java op zichzelf is niets; het zijn de ontwikkelaars die de taal hebben gemaakt tot wat het is. Precies dat wordt in de Java Tribe binnen Team Rockstars IT benadrukt.
+            </Title>
+            <br><br>
+          </div>
 
-                <div class="profileBox">
-                    <div>
-                        <img :src="require('assets/img/User.jpg')" class="avatar" />
-                    </div>
-                    <div>
-                        <Title size="1">
-                            Naam
-                        </Title>
-                        <Title size="3">
-                            Software Developer
-                        </Title>
-                    </div>
-                </div>
+          <div class="profileBox">
+            <div>
+              <img :src="require('assets/img/User.jpg')" class="avatar" />
             </div>
+            <div>
+              <Title size="1">
+                Naam
+              </Title>
+              <Title size="3">
+                Software Developer
+              </Title>
+            </div>
+          </div>
         </div>
+      </div>
+
+
+
     </div>
+
 
     <!--<div class="hero-image mt-16" v-bind:style="{ backgroundImage: 'url(' + require('assets/img/Buildings.jpg') + ')' }">
         <Carousel />
     </div>-->
     <div class="hero-image mt-10" >
-      <img class="hero-banner" :src="require('assets/img/Buildings.jpg')">  
+      <img class="hero-banner" :src="require('assets/img/Buildings.jpg')">
       <Carousel />
     </div>
-</div>
+  </div>
 
 </template>
 
 <script>
 export default {
-    name: 'TribeDescription'
+  name: 'TribeDescription'
 }
 </script>
 
@@ -55,21 +59,21 @@ export default {
 @import "assets/css/variables";
 
 .avatar {
-    border-radius: 50%;
-    width: 200px;
-    height: 200px;
-    object-fit: cover;
-    margin-left: auto;
-    margin-right: auto;
+  border-radius: 50%;
+  width: 200px;
+  height: 200px;
+  object-fit: cover;
+  margin-left: auto;
+  margin-right: auto;
 }
 
 .profileBox {
-    margin: auto;
-    text-align: center;
+  margin: auto;
+  text-align: center;
 }
 
 // .hero-image {
-    
+
 //     background-size: cover;
 //     background-position: center;
 //     background-repeat: no-repeat;
