5ef118d2253b5a92e20a911f6fa0c105c354283a
Jesse Antoni <i489617@fhict.nl>
2022-04-13 12:30:42 +0200

    Commented out button animation on hover, added svg files to buttons

--- components/Button.vue
+++ components/Button.vue
@@ -2,22 +2,24 @@
   <button
     :class="{
       'p-4 px-6 duration-500 ease-in-out  font-semibold': true,
-      'hover:p-8': status === 'normal',
+      //'hover:p-8': status === 'normal',
       'bg-rockstar-yellow text-black': theme === 'default',
       'bg-black text-white': theme === 'dark',
       'bg-white text-black': theme === 'light'
     }"
   >
-    <slot />
-    <span v-show="status === 'normal'">
-      &gt;&gt;
+    <div class="flex items-center">
+      <slot />
+      <span v-show="status === 'normal'">
+      <img :src="require('assets/img/icons/forward-solid.svg')" class="icon-size ml-2"/>
     </span>
-    <span v-show="status === 'loading'">
-      o
+      <span v-show="status === 'loading'">
+      <img :src="require('assets/img/icons/loader.svg')" class="icon-size ml-2"/>
     </span>
-    <span v-show="status === 'active'">
-      &#10003;
+      <span v-show="status === 'active'">
+      <img :src="require('assets/img/icons/check-solid.svg')" class="icon-size ml-2"/>
     </span>
+    </div>
   </button>
 </template>
 
@@ -42,5 +44,10 @@ export default {
 
 button {
   font-family: DIN2014-Bold, sans-serif;
+}
+
+.icon-size {
+  width: 0.75rem;
+  height: 0.75rem;
 }
 </style>
