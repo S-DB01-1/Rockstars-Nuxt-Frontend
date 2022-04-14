635551ec1009196767e46e8dbcd19a52c45d631b
Fabian te Raaij <fabian.teraaij@student.fontys.nl>
2022-04-14 14:10:36 +0200

    Add podcast page and SpotifySmall component

---
+++ components/SpotifySmall.vue
@@ -0,0 +1,25 @@
+<template>
+  <main>
+    <section>
+      <iframe style="border-radius:12px"
+              :src="spotify_url" width="100%"
+              height="152" frameBorder="0" allowfullscreen=""
+              allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+    </section>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "SpotifySmall",
+  props: {
+    spotify_url: {
+      type: String
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
