47b5b768507dff4a761d930560e4ca88dde35502
Fabian te Raaij <fabian.teraaij@student.fontys.nl>
2022-05-11 14:52:35 +0200

    Make Spotify.vue component dynamic

--- components/Dumb/Atoms/Spotify.vue
+++ components/Dumb/Atoms/Spotify.vue
@@ -5,7 +5,7 @@
               allowtransparency="true"
               frameborder="0"
               height="232"
-              src="https://open.spotify.com/embed-podcast/episode/7d9QeMg8T5XY5y85gyp0wb"
+              :src="spotify_url"
               width="100%"></iframe>
     </section>
   </main>
@@ -13,8 +13,13 @@
 
 <script>
 export default {
-  name: 'spotify'
-}
+  name: 'spotify',
+  props: {
+    spotify_url: {
+      type: String
+    },
+  },
+};
 </script>
 
 <style scoped>
