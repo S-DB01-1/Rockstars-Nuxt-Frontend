8bfc18aa1bf760454bd9e0df21be28a9191971e3
Mex Steens <m.steens@student.fontys.nl>
2022-05-12 11:11:01 +0200

    removed comments + small errors

--- components/Carousel.vue
+++ components/Carousel.vue
@@ -2,59 +2,11 @@
   <div class="hero-image mt-10 relative">
     <div class="container">
       <client-only>
-      <div v-if="!isEmpty(articles)">
-          <VueSlickCarousel v-bind="settings">
-                                                                <!-- <div v-for="article in articles" :key="article">
-                                                                  <Card Author=article.Author Title=article.Name Description=article.Description />
-                                                                </div> -->
-            <div v-for="article in articles" :key="article">
-              <Card :Author=article.title :Title=article.title :Description=article.description :TribeId=article.tribeid :ArticleId=article.id />
-
-                                                                                      <!-- <div class="rounded overflow-hidden shadow-lg m-8 bg-white">
-                                                                                        <div class="bg-rockstar-yellow author-block -mt-4 ml-4 pl-4 pr-4 pb-2 pt-2">
-                                                                                          <p>{{article.title}}</p>
-                                                                                          </div>
-                                                                                          <div>
-                                                                                          <img class="w-full thumbnail" :src="require('assets/img/Banner.png')">
-                                                                                          </div>
-                                                                                          <div class="px-6 py-4">
-                                                                                            <Title size="3" class="font-bold mb-2">
-                                                                                              test
-                                                                                            </Title>
-                                                                                            <p class="text-gray-700 text-xs">
-                                                                                              tests
-                                                                                            </p>
-                                                                                          </div>
-                                                                                          <div>
-                                                                                            <Button theme="light">
-                                                                                              GA NAAR
-                                                                                            </Button>
-                                                                                          </div>
-                                                                                        </div> -->
-              </div>  
-                                                            <!--            <div v-for="article in articles">-->
-                                                            <!--              <div class="rounded overflow-hidden shadow-lg m-8 bg-white">-->
-                                                            <!--                <div class="bg-rockstar-yellow author-block -mt-4 ml-4 pl-4 pr-4 pb-2 pt-2">-->
-                                                            <!--                  <p>{{ article.Author }}</p>-->
-                                                            <!--                </div>-->
-                                                            <!--                <div>-->
-                                                            <!--                  <img class="w-full thumbnail" :src="require('assets/img/Banner.png')">-->
-                                                            <!--                </div>-->
-                                                            <!--                <div class="px-6 py-4">-->
-                                                            <!--                  <Title size="3" class="font-bold mb-2">-->
-                                                            <!--                    {{ article.Name }}-->
-                                                            <!--                  </Title>-->
-                                                            <!--                  <p class="text-gray-700 text-xs">-->
-                                                            <!--                    {{ article.Description }}-->
-                                                            <!--                  </p>-->
-                                                            <!--                </div>-->
-                                                            <!--                <div>-->
-                                                            <!--                  <Button theme="light">-->
-                                                            <!--                    GA NAAR-->
-                                                            <!--                  </Button>-->
-                                                            <!--                </div>-->
-                                                            <!--              </div>-->
-                                                            <!--            </div>-->
+        <div v-if="!isEmpty(articles)">
+          <VueSlickCarousel v-bind="settings">                                                          
+            <div v-for="(article, index) in articles" :key="index">
+              <Card :Author=article.title :Title=article.title :Description=article.description :TribeId=String(article.tribeid) :ArticleId=String(article.id) />                                                                                  
+            </div>  
           </VueSlickCarousel>
         </div>
       </client-only>
