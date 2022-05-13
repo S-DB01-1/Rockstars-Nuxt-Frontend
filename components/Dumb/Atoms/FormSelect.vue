e8955feb65d5dad19b09fe93f57cdc8961e28709
Stijn van den Berg <489789@student.fontys.nl>
2022-05-13 10:14:33 +0200

    Finished Button styling Finished FormInput styling Changed HTML head meta favicons

--- components/Dumb/Atoms/FormSelect.vue
+++ components/Dumb/Atoms/FormSelect.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="form-element">
+<<<<<<<< HEAD:components/Dumb/Atoms/FormSelect.vue
     <select :value="value" @change="$emit('change', $event.target.value)" :class="{
       'bg-gray-100 m-2 border-b-2 border-gray-400 text-gray-700 hover:text-black': true,
       'border-red-400': status === 'error'
@@ -8,6 +9,18 @@
         {{ choice }}
       </option>
     </select>
+========
+    <input :type="type" :placeholder="placeholder" :value="value" @input="$emit('input', $event.target.value)" :class="{
+      'bg-gray-100 m-2 border-b-2 border-gray-400 text-gray-700 hover:text-black': true,
+      'border-red-400': status === 'error'
+    }">
+    <span v-if="status === 'correct'" class="symbol">
+      <img :src="require('assets/img/icons/check-black.svg')" class="w-4 ml-2 absolute -left-12 top-7"/>
+    </span>
+    <span v-else-if="status === 'error'" class="symbol">
+      <img :src="require('assets/img/icons/cross-red.svg')" class="w-4 ml-2 absolute -left-12 top-6"/>
+    </span>
+>>>>>>>> 82a40eb (Finished Button styling):components/Dumb/Atoms/FormInput.vue
   </div>
 </template>
 
@@ -16,7 +29,15 @@ export default {
   name: 'Forms',
   props: {
     value: String | Number | Date,
+<<<<<<<< HEAD:components/Dumb/Atoms/FormSelect.vue
     choices: Array,
+========
+    type: {
+      type: String,
+      default: 'text'
+    },
+    placeholder: String,
+>>>>>>>> 82a40eb (Finished Button styling):components/Dumb/Atoms/FormInput.vue
     status: {
       type: String,
       default: 'normal'
