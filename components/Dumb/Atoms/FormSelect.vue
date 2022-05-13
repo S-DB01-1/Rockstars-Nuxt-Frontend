a213932a4c2999184a7e20419e48d38d5ace07e8
Stijn van den Berg <489789@student.fontys.nl>
2022-05-13 10:14:33 +0200

    Finished Button styling Finished FormInput styling Changed HTML head meta favicons

--- components/Dumb/Atoms/FormSelect.vue
+++ components/Dumb/Atoms/FormSelect.vue
@@ -1,15 +1,13 @@
 <template>
   <div class="form-element">
-    <input :type="type" :placeholder="placeholder" :value="value" @input="$emit('input', $event.target.value)" :class="{
+    <select :value="value" @change="$emit('change', $event.target.value)" :class="{
       'bg-gray-100 m-2 border-b-2 border-gray-400 text-gray-700 hover:text-black': true,
       'border-red-400': status === 'error'
     }">
-    <span v-if="status === 'correct'" class="symbol">
-      <img :src="require('assets/img/icons/check-black.svg')" class="w-4 ml-2 absolute -left-12 top-7"/>
-    </span>
-    <span v-else-if="status === 'error'" class="symbol">
-      <img :src="require('assets/img/icons/cross-red.svg')" class="w-4 ml-2 absolute -left-12 top-6"/>
-    </span>
+      <option v-for="(choice, index) in choices" :key="index">
+        {{ choice }}
+      </option>
+    </select>
   </div>
 </template>
 
@@ -18,11 +16,7 @@ export default {
   name: 'Forms',
   props: {
     value: String | Number | Date,
-    type: {
-      type: String,
-      default: 'text'
-    },
-    placeholder: String,
+    choices: Array,
     status: {
       type: String,
       default: 'normal'
@@ -35,7 +29,7 @@ export default {
 .form-element {
   display: inline-block;
 
-  input {
+  select {
     width: 400px;
     cursor: pointer;
     padding: 1rem;
--- pages/styling.vue
+++ pages/styling.vue
@@ -46,26 +46,34 @@
       </Button>
     </section>
     <section class="p-4">
-      <Title size="1">
-        FORMS
+      <Title size="1" theme="light">
+        Forms
       </Title>
-      <FormInput type="text" />
-      <FormInput status="error" />
+      <FormInput v-model="testvar" type="text" />
+      <FormInput v-model="testvar" status="correct" />
+      <FormInput v-model="testvar" status="error" />
+      <FormSelect v-model="testList" :choices="[1, 2, 3]" />
     </section>
   </main>
 </template>
 
 <script>
-import Button from "~/components/Dumb/Atoms/Button"
-import Title from "~/components/Dumb/Atoms/Title"
+import Button from "@/components/Dumb/Atoms/Button";
+import Title from "@/components/Dumb/Atoms/Title";
+import FormInput from "@/components/Dumb/Atoms/FormInput";
+import FormSelect from "@/components/Dumb/Atoms/FormSelect";
+
 export default {
-  name: "styling",
-  components: {
-    Button,
-    Title,
-  },
-};
+  components: {Button, Title, FormInput, FormSelect},
+  data() {
+    return {
+      testvar: null,
+      testList: null
+    }
+  }
+}
 </script>
+
 <style scoped>
 
 </style>
