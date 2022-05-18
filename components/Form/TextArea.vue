bd015e7e00ea4f4ff84ee63443634ec47b54c1a9
Stijn van den Berg <489789@student.fontys.nl>
2022-05-18 09:45:12 +0200

    New Forms stylings

---
+++ components/Form/TextArea.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="form-element w-full">
+    <textarea
+      :value="value"
+      :placeholder="placeholder"
+      @input="$emit('input', $event.target.value)"
+      :maxlength="max"
+      :minlength="min"
+      class="bg-gray-100 m-2 border-b-2 border-gray-400 text-gray-700 hover:text-black">
+    </textarea>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TextArea",
+  props: {
+    value: String,
+    placeholder: String,
+    max: {
+      type: Number,
+      default: 256
+    },
+    min: {
+      type: Number,
+      default: 0
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.form-element {
+  textarea {
+    width: calc(100% - 1rem);
+    cursor: pointer;
+    padding: 1rem;
+
+    &:hover {
+      padding-bottom: calc(1rem - 2px);
+      border-bottom-width: 4px;
+    }
+  }
+}
+</style>
